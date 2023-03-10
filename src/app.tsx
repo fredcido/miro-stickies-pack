241489f777964c3d809aae303f8ffc2e244192df
Frederico Estrela <frederico.e@miro.com>
2023-03-10 23:44:05 +0100

    Create packs (#1)

--- src/app.tsx
+++ src/app.tsx
@@ -1,45 +1,195 @@
-import * as React from 'react';
-import {createRoot} from 'react-dom/client';
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+import { StickyNoteColor } from "@mirohq/websdk-types";
+import classnames from "classnames";
 
-async function addSticky() {
-  const stickyNote = await miro.board.createStickyNote({
-    content: 'Hello, World!',
-  });
-
-  await miro.board.viewport.zoomTo(stickyNote);
-}
+import { defaultConfig, saveConfig, getConfig, createPack } from "./pack";
+import type { PackConfig } from "./pack";
 
 const App: React.FC = () => {
+  const [config, setConfig] = React.useState<PackConfig>(defaultConfig);
+
   React.useEffect(() => {
-    addSticky();
+    const fetchSavedConfig = async () => {
+      const existingConfig = await getConfig();
+      if (existingConfig) {
+        setConfig((data) => ({ ...data, ...existingConfig }));
+      }
+    };
+
+    fetchSavedConfig();
   }, []);
 
+  const set = React.useCallback(
+    <K extends keyof PackConfig>(key: K, value: PackConfig[K]) => {
+      setConfig((data) => ({
+        ...data,
+        [key]: value,
+      }));
+    },
+    []
+  );
+
+  const handleChange = React.useCallback(
+    <K extends keyof PackConfig>(key: K) =>
+      (ev: React.FormEvent<HTMLInputElement>) => {
+        const { value } = ev.currentTarget;
+        // @ts-expect-error
+        set(key, +value);
+      },
+    [set]
+  );
+
+  const handleColor = React.useCallback(
+    (color: StickyNoteColor) => {
+      const { colors } = config;
+      if (colors.includes(color)) {
+        const newColors = colors.filter((c) => c !== color);
+        setConfig((config) => ({
+          ...config,
+          colors: newColors,
+        }));
+      } else {
+        setConfig((config) => ({
+          ...config,
+          colors: [...config.colors, color],
+        }));
+      }
+    },
+    [setConfig, config]
+  );
+
+  const handleReset = React.useCallback(() => {
+    const reset = async () => {
+      await saveConfig(defaultConfig);
+      setConfig(defaultConfig);
+    };
+    reset();
+  }, [setConfig]);
+
+  const handleFormSubmit = React.useCallback(
+    (ev: React.FormEvent<HTMLFormElement>) => {
+      ev.preventDefault();
+
+      const save = async () => {
+        await saveConfig(config);
+        await createPack({ config });
+      };
+
+      save();
+
+      return false;
+    },
+    [config]
+  );
+
   return (
-    <div className="grid wrapper">
-      <div className="cs1 ce12">
-        <img src="/src/assets/congratulations.png" alt="" />
+    <form onSubmit={handleFormSubmit}>
+      <div className="form-group">
+        <label htmlFor="packs">Packs</label>
+        <div className="input-group">
+          <input
+            className="input input-small"
+            type="number"
+            placeholder="e.g. 5"
+            name="packs"
+            value={config.packs}
+            onChange={handleChange("packs")}
+          />
+        </div>
       </div>
-      <div className="cs1 ce12">
-        <h1>Congratulations!</h1>
-        <p>You've just created your first Miro app!</p>
-        <p>
-          To explore more and build your own app, see the Miro Developer
-          Platform documentation.
-        </p>
+
+      <div className="form-group">
+        <label htmlFor="stickies">Stickies</label>
+        <div className="input-group">
+          <input
+            className="input input-small"
+            type="number"
+            placeholder="e.g. 5"
+            name="stickies"
+            value={config.stickies}
+            onChange={handleChange("stickies")}
+          />
+        </div>
       </div>
-      <div className="cs1 ce12">
-        <a
-          className="button button-primary"
-          target="_blank"
-          href="https://developers.miro.com"
-        >
-          Read the documentation
-        </a>
+
+      <div className="form-group">
+        <label htmlFor="stickyOffset">Sticky offset</label>
+        <div className="input-group">
+          <input
+            className="input input-small"
+            type="number"
+            placeholder="e.g. 5"
+            name="stickyOffset"
+            value={config.stickyOffset}
+            onChange={handleChange("stickyOffset")}
+          />
+        </div>
       </div>
-    </div>
+
+      <div className="form-group">
+        <label htmlFor="stickyGap">Sticky gap</label>
+        <div className="input-group">
+          <input
+            className="input input-small"
+            type="number"
+            placeholder="e.g. 5"
+            name="stickyGap"
+            value={config.stickyGap}
+            onChange={handleChange("stickyGap")}
+          />
+        </div>
+      </div>
+      <div className="form-group">
+        <label htmlFor="stickyGap">Colors</label>
+        <div className="colors">
+          {defaultConfig.colors.map((color) => (
+            <button
+              title={color}
+              key={color}
+              type="button"
+              style={{ backgroundColor: color.replace("_", "") }}
+              onClick={() => handleColor(color)}
+              className={classnames("color", {
+                active: config.colors.includes(color),
+              })}
+            ></button>
+          ))}
+        </div>
+      </div>
+
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          checked={config.selectItems}
+          onChange={(ev) => set("selectItems", ev.target.checked)}
+        />
+        <span>Select items</span>
+      </label>
+
+      <label className="checkbox">
+        <input
+          type="checkbox"
+          checked={config.zoomTo}
+          onChange={(ev) => set("zoomTo", ev.target.checked)}
+        />
+        <span>Zoom to pack</span>
+      </label>
+
+      <button className="button button-primary" type="submit">
+        Create
+      </button>
+      <button
+        className="button button-secondary"
+        type="reset"
+        onClick={handleReset}
+      >
+        Reset default values
+      </button>
+    </form>
   );
 };
 
-const container = document.getElementById('root');
+const container = document.getElementById("root")!;
 const root = createRoot(container);
 root.render(<App />);
