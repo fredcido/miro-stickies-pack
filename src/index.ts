8d2de52c9d837a7b74d33bf7a3e3064a6f27b10c
Frederico Estrela Gaiva <frederico.e@miro.com>
2023-04-03 09:35:04 +0200

    Does not use config from custom action

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import type { StickyNote, CustomEvent } from "@mirohq/websdk-types";
-import { getReferenceItem, createPack, getConfig } from "./pack";
+import { getReferenceItem, createPack } from "./pack";
 
 async function init() {
   miro.board.ui.on("icon:click", async () => {
     await miro.board.ui.openPanel({ url: "app.html" });
   });
 
-  miro.board.ui.on("custom:create-pack", async ({ items }: CustomEvent) => {
+  miro.board.ui.on("custom:create-pack", ({ items }: CustomEvent) => {
     const referenceItem = getReferenceItem(items) as StickyNote;
-    const config = await getConfig();
-    createPack({ referenceItem, config });
+    createPack({ referenceItem });
   });
 
   miro.board.ui.on("custom:open-settings", async () => {
